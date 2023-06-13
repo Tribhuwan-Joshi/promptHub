05093f422dd8135ea84faa668d67ec28f166a154
Tribhuwan <mathincosmos@gmail.com>
2023-06-13 23:50:31 +0530

    add prompt form component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -11,7 +11,27 @@ const [post,setPost] = useState({
     tag: '',
 });
 const createPrompt = async (e) =>{
-
+e.preventDefault();
+setSubmitting(true);
+try{
+const response = await fetch('/api/prompt/new' , {
+    method : 'POST' , 
+    body : JSON.stringify({
+        prompt:post.prompt,
+        userId: session?.user.id,
+        tag:post.tag
+    })
+})
+if(response.ok){
+    router.push('/');
+}
+}
+catch(error){
+console.log(error);
+}
+finally{
+    setSubmitting(false);
+}
 }
   return <Form  type="Create" post={post} setPost={setPost}  submitting={submitting} handleSubmit={createPrompt} />
 
